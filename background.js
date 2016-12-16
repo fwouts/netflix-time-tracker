2c2c383b81efa981b9c410c43262b369379af35b
François Wouts <francois.wouts@gmail.com>
2016-12-16 18:46:41 +1100

    Make sure to count for the current day.

--- background.js
+++ background.js
@@ -1,30 +1,38 @@
 // Background script that listens for messages coming from injected.js and
 // updates the watch time in the badge.
 
-var today = new Date();
-today.setHours(0, 0, 0, 0);
-var todayKey = today.toString();
-console.log(todayKey);
-var timeElapsedSeconds;
+var todayKey;
+var secondsPerDate = {};
+
+function refreshTodayKey() {
+  var today = new Date();
+  today.setHours(0, 0, 0, 0);
+  todayKey = today.toString();
+}
+// Refresh the current date once a minute + on initialization.
+setInterval(refreshTodayKey, 60 * 1000);
+refreshTodayKey();
+
+chrome.runtime.onMessageExternal.addListener(onMessageReceived);
 chrome.storage.sync.get('seconds_per_date', function(value) {
-  var secondsPerDate = value['seconds_per_date'] || {};
-  timeElapsedSeconds = secondsPerDate[todayKey] || 0;
+  secondsPerDate = value['seconds_per_date'] || {};
+  var timeElapsedSeconds = secondsPerDate[todayKey] || 0;
   console.log("Initial spent time for today: " + timeElapsedSeconds);
-  chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    if (request.playing !== null) {
-      if (request.playing) {
-        timeElapsedSeconds++;
-        secondsPerDate[todayKey] = timeElapsedSeconds;
-        var minutes = parseInt(timeElapsedSeconds / 60);
-        chrome.browserAction.setBadgeText({
-          text: '' + minutes
-        });
-        chrome.browserAction.setBadgeBackgroundColor({
-          color: '#E33'
-        });
-        chrome.storage.sync.set({'seconds_per_date': secondsPerDate}, null);
-        console.log("Spent time: " + timeElapsedSeconds);
-      }
-    }
-  });
 });
+
+function onMessageReceived(request, sender, sendResponse) {
+  if (request.playing !== null) {
+    if (request.playing) {
+      secondsPerDate[todayKey] = (secondsPerDate[todayKey] || 0) + 1;
+      var minutes = parseInt(secondsPerDate[todayKey] / 60);
+      chrome.browserAction.setBadgeText({
+        text: '' + minutes
+      });
+      chrome.browserAction.setBadgeBackgroundColor({
+        color: '#E33'
+      });
+      chrome.storage.sync.set({'seconds_per_date': secondsPerDate}, null);
+      console.log("Spent time: " + secondsPerDate[todayKey]);
+    }
+  }
+}
